27099ca87b0f1a5ab82b525f2e890b3d6692734b
harshit7217 <harshitchaudhary568@gmail.com>
2026-08-08 21:45:08 +0530

    Develop a simple printer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        SimplerPrinter simplerPrinter = new SimplerPrinter();
+        simplerPrinter.print();
     }
 }
---
+++ src/SimplerPrinter.java
@@ -0,0 +1,9 @@
+public class SimplerPrinter {
+    public void print(){
+        System.out.println("I love animals!");
+        System.out.println("Let's check on the animals...");
+        System.out.println("The deer looks fine.");
+        System.out.println("The bat looks happy.");
+        System.out.println("The lion looks healthy");
+    }
+}
